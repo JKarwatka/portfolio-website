1535bf133771fdca3676b0836635cbb76099397d
Jakub Karwatka <jakubkarwatka@gmail.com>
2021-09-06 18:37:47 +0200

    init commit

---
+++ src/components/wrapper/index.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from "@test";
+
+import { Wrapper } from "./index";
+
+describe("Wrapper component testing with testing-library", () => {
+
+    const component = render(<Wrapper />);
+
+    it("renders without crashing", () => {
+        expect(component).toBeTruthy();
+    });
+});
