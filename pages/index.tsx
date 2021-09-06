e11efba2076e4ab315e0152eaf513219b2707878
Jakub Karwatka <jakubkarwatka@gmail.com>
2021-09-06 18:55:28 +0200

    cleanup after repository setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 
-import { Wrapper, Header, Main, Footer, Cards } from "@components";
-import GlobalStyle from "@styles/globalStyles";
-
 const Home: React.FC = () => {
   return (
-        <Wrapper>
-            <GlobalStyle />
-            <Header />
-            <Main />
-            <Cards />
-            <Footer />
-        </Wrapper>
+        <div>Hello There!</div>
     );
 }
 export default Home;
