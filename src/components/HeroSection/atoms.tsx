3ba6d268eda9f954b5af0ad2b5dbbaa6bb9fb26b
Jakub Karwatka <jakubkarwatka@gmail.com>
2021-09-18 22:29:05 +0200

    Replace background image

--- src/components/HeroSection/atoms.tsx
+++ src/components/HeroSection/atoms.tsx
@@ -2,9 +2,7 @@ import { Box } from "@mui/system";
 import styled from "styled-components";
 
 export const HeroSectionBackground = styled(Box)`
-  display: flex;
-  align-items: center;
   min-height: 100vh;
-  background: url("images/galaxy.jpg") no-repeat center center;
+  background: url("images/hero-background.jpg") no-repeat center center;
   background-size: 100% auto;
 `;
